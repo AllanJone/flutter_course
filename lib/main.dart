1bce331fa3b53cbccebf5ba4dad5f63a159348e7
Allan Jone <joeallan123@gmail.com>
2019-05-23 12:35:54 +0530

    Adding the Basic Auth Page & Replacing Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/pages/home.dart';
+import 'package:flutter_course/pages/auth.dart';
 //import 'package:flutter/rendering.dart';
 
 void main() {
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //   primarySwatch: Colors.red,
       // ),
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
