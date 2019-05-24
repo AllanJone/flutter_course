f2a659a3cb482145a1053a47b0d41316d480c51c
Allan Jone <joeallan123@gmail.com>
2019-05-24 12:25:18 +0530

    Formated Login Page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -17,46 +17,69 @@ class _AuthPageState extends State<AuthPage> {
         title: Text('Login'),
       ),
       body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.3), BlendMode.dstATop),
+            image: AssetImage('assets/background.jpg'),
+          ),
+        ),
         padding: EdgeInsets.all(15.0),
-        child: ListView(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'E-mail'),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String value) {
-                setState(() {
-                  _emailValue = value;
-                });
-              },
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    filled: true,
+                    fillColor: Colors.black.withOpacity(0.3),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (String value) {
+                    setState(() {
+                      _emailValue = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 10.9,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    filled: true,
+                    fillColor: Colors.black.withOpacity(0.3),
+                  ),
+                  obscureText: true,
+                  onChanged: (String value) {
+                    setState(() {
+                      _passwordValue = value;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text('Accept Terms'),
+                  onChanged: (bool value) {
+                    setState(() {
+                      _acceptTerms = value;
+                    });
+                  },
+                  value: _acceptTerms,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/products');
+                  },
+                  child: Text('LOGIN'),
+                ),
+              ],
             ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              onChanged: (String value) {
-                setState(() {
-                  _passwordValue = value;
-                });
-              },
-            ),
-            SwitchListTile(
-              title: Text('Accept Terms'),
-              onChanged: (bool value) {
-                setState(() {
-                  _acceptTerms = value;
-                });
-              },
-              value: _acceptTerms,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/products');
-              },
-              child: Text('LOGIN'),
-            ),
-          ],
+          ),
         ),
       ),
     );
