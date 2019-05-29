d26dad60741445a7decfa5294a05c175ce30bb23
Allan Jone <joeallan123@gmail.com>
2019-05-29 20:00:41 +0530

    Refactoring into widgets

---
+++ lib/widgets/products/product_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../ui_elements/title_default.dart';
+import 'price_tag.dart';
+import '../products/address_tag.dart';
+
+class ProductCard extends StatelessWidget {
+  final Map<String, dynamic> product;
+  final int productIndex;
+
+  ProductCard(this.product, this.productIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(product['image']),
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: TitleDefault(
+                    product['title'],
+                  ),
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                PriceTag(product['price'].toString()),
+              ],
+            ),
+          ),
+          AddressTag('Union Square, San Fransisco'),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.info),
+                  color: Colors.deepPurple,
+                  onPressed: () => Navigator.pushNamed<bool>(
+                      context, '/product/' + productIndex.toString())),
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                color: Colors.red,
+                onPressed: () => Navigator.pushNamed<bool>(
+                    context, '/product/' + productIndex.toString()),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
