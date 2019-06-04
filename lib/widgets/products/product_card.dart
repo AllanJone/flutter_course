b5a2db44995b08efe35620da59e8fc9405340575
Allan Jone <joeallan123@gmail.com>
2019-06-04 13:04:22 +0530

    Adding fadein to Image

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -63,7 +63,12 @@ class ProductCard extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
-          Image.network(product.image),
+          FadeInImage(
+            image: NetworkImage(product.image),
+            height: 300.0,
+            fit: BoxFit.cover,
+            placeholder: AssetImage('assets/food.jpg'),
+          ),
           Container(
             padding: EdgeInsets.only(top: 10.0),
             child: _buildTitlePriceRow(),
